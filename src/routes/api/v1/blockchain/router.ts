638e6390ca86069b59fb74581a26687e89045c03
Anant Sharma <anantatnet@gmail.com>
2019-07-06 23:56:04 +0530

    Updated Get Blockchain endpoint contract

--- src/routes/api/v1/blockchain/router.ts
+++ src/routes/api/v1/blockchain/router.ts
@@ -14,7 +14,10 @@ const router = express.Router();
  */
 router.get('/', (req: express.Request, res: express.Response) => {
     try {
-        res.status(200).json(blockchain);
+        res.status(200).json({
+            Chain: blockchain.getChain(),
+            PendingTransactions: blockchain.getPendingTransactions(),
+        });
     } catch (e) {
         res.status(400).json({
             error: e,
